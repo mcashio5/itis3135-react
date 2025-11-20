b50ffcf43e6e236bb8ed8cade07c758ee4998d83
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:30:02 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
-import Layout from './Layout.jsx'
-import App from './App.jsx'
-import Introduction from './Introduction.jsx'
+import Layout from './Layout.jsx';
+import App from './App.jsx';
+import Introduction from './Introduction.jsx';
+import Contract from './Contract.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/' element={<App />}></Route>
-          <Route path='/introduction' element={<Introduction />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="contract" element={<Contract />} />
+          <Route path="introduction" element={<Introduction />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
