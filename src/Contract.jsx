7515f63788731d0614192c35fb70fd7c9a705c86
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:54:28 -0500

    Update Contract.jsx

--- src/Contract.jsx
+++ src/Contract.jsx
@@ -1,22 +1,26 @@
-import { useEffect } from 'react';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 export default function Contract() {
-  useEffect(() => {
-    document.title = 'Course Contract – ITIS 3135 – Michael Cashion';
-  }, []);
-
   return (
     <>
-      <h2>Course Contract</h2>
-      <p className="tagline">
-        Commitment to course policies and academic integrity.
-      </p>
+      <Header />
 
-      <p>
-        I, Michael Cashion, agree to abide by the rules, policies, and standards
-        of ITIS 3135. I will submit my own work, cite sources, and follow all
-        timelines and accessibility/usability guidelines for web content.
-      </p>
+      <main>
+        <h2>Course Contract</h2>
+        <p className="tagline">
+          Commitment to course policies and academic integrity.
+        </p>
+
+        <p>
+          I, Michael Cashion, agree to abide by the rules, policies, and
+          standards of ITIS 3135. I will submit my own work, cite sources, and
+          follow all timelines and accessibility/usability guidelines for web
+          content.
+        </p>
+      </main>
+
+      <Footer />
     </>
   );
 }
