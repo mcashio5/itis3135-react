7071ba8d36b7c5b2dd3cefb4261ce942fe01a442
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:50:52 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,35 +2,13 @@ export default function Footer() {
   return (
     <footer>
       <nav>
-        <a href="https://github.com/mcashio5" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-        <a href="https://mcashio5.github.io/" target="_blank" rel="noreferrer">
-          GitHub Pages
-        </a>
-        <a
-          href="https://webpages.charlotte.edu/mcashio5/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CLT Web Home
-        </a>
-        <a
-          href="https://validator.w3.org/nu/?doc=referer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          HTML Validation
-        </a>
-        <a
-          href="https://jigsaw.w3.org/css-validator/validator?uri=referer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CSS Validation
-        </a>
+        <a href="https://github.com/mcashio5" target="_blank">GitHub</a>
+        <a href="https://mcashio5.github.io/" target="_blank">GitHub Pages</a>
+        <a href="https://webpages.charlotte.edu/mcashio5/" target="_blank">CLT Web Home</a>
+        <a href="https://validator.w3.org/nu/?doc=referer" target="_blank">HTML Validation</a>
+        <a href="https://jigsaw.w3.org/css-validator/validator?uri=referer" target="_blank">CSS Validation</a>
       </nav>
-      <p>&copy; 2025 Michael Cashion</p>
+      <p>© 2025 Michael Cashion</p>
     </footer>
   );
 }
