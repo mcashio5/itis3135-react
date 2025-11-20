6092a38a9db86ae745941104cde3e1f65082e3d4
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:50:11 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
+    <header className="site-header">
       <h1>Michael Cashion – ITIS 3135</h1>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/contract">Course Contract</Link>
-        <Link to="/introduction">Introduction</Link>
+      <nav className="main-nav">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contract">Course Contract</Link></li>
+          <li><Link to="/introduction">Introduction</Link></li>
+        </ul>
       </nav>
     </header>
   );
