ba40ffbdaa3079b048fc50b033ef1e91c407fc96
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:54:11 -0500

    Update Introduction.jsx

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from 'react';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 export default function Introduction() {
-  useEffect(() => {
-    document.title = 'Introduction – ITIS 3135 – Michael Cashion';
-  }, []);
-
   return (
     <>
-      <h2>Introduction</h2>
-      <p className="tagline">Who I am, what I do, and what I’m learning.</p>
+      <Header />
 
-      <p>
-        Hi! I’m Michael Cashion, a Computer Science student at UNC Charlotte
-        (’26). I enjoy web apps, cloud/Azure projects, and building clean,
-        accessible interfaces. Outside class, I lift, play intramural sports,
-        and tinker with hardware/software builds.
-      </p>
+      <main>
+        <h2>Introduction</h2>
+        <p className="tagline">Who I am, what I do, and what I’m learning.</p>
+
+        <p>
+          Hi! I’m Michael Cashion, a Computer Science student at UNC Charlotte
+          (’26). I enjoy web apps, cloud/Azure projects, and building clean,
+          accessible interfaces. Outside class, I lift, play intramural sports,
+          and tinker with hardware/software builds.
+        </p>
+      </main>
+
+      <Footer />
     </>
   );
 }
