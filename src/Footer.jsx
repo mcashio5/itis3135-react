c2fcc1dc69627ef349b27079a25e0854c8f1cc05
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:27:57 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,36 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Designed by Michael Cashion</p>
+      <nav>
+        <a href="https://github.com/mcashio5" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+        <a href="https://mcashio5.github.io/" target="_blank" rel="noreferrer">
+          GitHub Pages
+        </a>
+        <a
+          href="https://webpages.charlotte.edu/mcashio5/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CLT Web Home
+        </a>
+        <a
+          href="https://validator.w3.org/nu/?doc=referer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          HTML Validation
+        </a>
+        <a
+          href="https://jigsaw.w3.org/css-validator/validator?uri=referer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CSS Validation
+        </a>
+      </nav>
+      <p>&copy; 2025 Michael Cashion</p>
     </footer>
   );
 }
