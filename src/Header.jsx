9560e64ea3002650adbfdcc7a6e805676f1d34d4
Michael Cashion <mcashio5@uncc.edu>
2025-11-19 21:49:43 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1>Michael Cashion's Mighty Chicken | ITIS 3135</h1>
+      <h1>Michael Cashion's Mighty Chicken lmao| ITIS 3135</h1>
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
