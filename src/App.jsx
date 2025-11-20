e670bde9ebdfebd13544540f4f8b6c3571717fa7
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 00:53:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 
 export default function App() {
-  useEffect(() => {
-    document.title = 'ITIS 3135 – Michael Cashion';
-  }, []);
-
   return (
     <>
-      <h2>Welcome</h2>
-      <p>This is my ITIS 3135 course homepage.</p>
+      <Header />
+
+      <main>
+        <h2>Welcome</h2>
+        <p>This is my ITIS 3135 course homepage.</p>
+      </main>
+
+      <Footer />
     </>
   );
 }
