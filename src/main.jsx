3d195ae9118e939a432abbe67bc8a347fa13bc20
Michael Cashion <mcashio5@uncc.edu>
2025-11-20 01:05:55 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Introduction from "./Introduction.jsx";
-import Contract from "./Contract.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/introduction" element={<Introduction />} />
-      <Route path="/contract" element={<Contract />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
