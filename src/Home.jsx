99ab8d9047557eaec41034d4e846c52cfb7885dd
Michael Cashion <mcashio5@uncc.edu>
2025-11-30 15:05:08 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,10 @@
 export default function Home() {
-  document.title += " || " + "Home";
   return (
-    <main>
-      <h2>Home</h2>
-      <p>Hello! Welcome to my 3135 course website</p>
-      <p>
-        This webpage was built using javascript to extend my knowledge of web deisgn
-      </p>
-    </main>
+    <div className="main-box">
+    <section className="main-card">
+      <h2>Welcome</h2>
+      <p>This is my ITIS 3135 course homepage.</p>
+    </div>
+    </section>
   );
 }
