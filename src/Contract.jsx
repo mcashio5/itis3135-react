ef2f2fdfa5237dd28b0a07a6d1d48ebee0870289
Michael Cashion <mcashio5@uncc.edu>
2025-11-30 13:27:34 -0500

    Update Contract.jsx

--- src/Contract.jsx
+++ src/Contract.jsx
@@ -1,9 +1,9 @@
-// src/Contract.jsx
 export default function Contract() {
+  document.title += " || " + "Contract";
   return (
-    <section className="main-card">
-      <h2>Course Contract</h2>
-      <p className="tagline">
+    <main>
+      <h2>Contract</h2>
+        <p className="tagline">
         Commitment to course policies and academic integrity.
       </p>
       <p>
@@ -11,6 +11,6 @@ export default function Contract() {
         of ITIS 3135. I will submit my own work, cite sources, and follow all
         timelines and accessibility/usability guidelines for web content.
       </p>
-    </section>
+    </main>
   );
 }
