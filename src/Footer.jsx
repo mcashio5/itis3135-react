5c7f62fa46393ae95f95c43c4c5d7b2896a24c7e
Michael Cashion <mcashio5@uncc.edu>
2025-11-30 13:25:56 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,49 +1,16 @@
-// src/Footer.jsx
-function Footer() {
-  return (
-    <footer className="site-footer">
-      <nav className="footer-nav">
-        <a href="https://github.com/mcashio5" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-        <a href="https://mcashio5.github.io/" target="_blank" rel="noreferrer">
-          GitHub Pages
-        </a>
-        <a
-          href="https://webpages.charlotte.edu/mcashio5/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          CLT Web Home
-        </a>
-        <a href="https://mcashio5.github.io/itis3135/" target="_blank" rel="noreferrer">
-          Course Page
-        </a>
-        <a
-          href="https://www.freecodecamp.org/mcashio5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          freeCodeCamp
-        </a>
-        <a
-          href="https://www.codecademy.com/profiles/mcashio5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Codecademy
-        </a>
-        <a
-          href="https://www.linkedin.com/in/michael-cashion-a891082b6/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          LinkedIn
-        </a>
-      </nav>
-      <p>&copy; 2025 Michael Cashion</p>
-    </footer>
-  );
+import { Link } from 'react-router-dom'
+export default function Footer() {
+    return (<footer>
+        <nav>
+            <p>
+            <a href="https://webpages.charlotte.edu/mcashio5/" target="_blank" rel="noopener noreferrer">CLT Web</a> ||
+            <a href="https://github.com/mcashio5/mcashio5.github.io" target="_blank" rel="noopener noreferrer">GitHub</a> ||
+            <a href="https://mcashio5.github.io/itis3135/" target="_blank" rel="noopener noreferrer">GitHub.io</a> ||
+            <a href="https://www.freecodecamp.org/mcashio5" target="_blank" rel="noopener noreferrer">freeCodeCamp</a> ||
+            <a href="https://www.codecademy.com/profiles/mcashio5" target="_blank" rel="noopener noreferrer">Codecademy</a> ||
+            <a href="https://www.linkedin.com/in/michael-cashion-a891082b6/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </p>
+        </nav>
+    <p>Website Desgined by <a href="https://mcashio5.github.io/itis3135/cashioninc.com/">Cashion Incorporated</a>&copy; 2025</p>
+    </footer>);
 }
-
-export default Footer;
