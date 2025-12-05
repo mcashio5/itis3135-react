0f2fbc86853e2392e56de0ca57f47ad2eadddc44
Michael Cashion <mcashio5@uncc.edu>
2025-12-04 22:46:28 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <>
-      <h1>Michael Cashions' Mighty Chicken || ITIS3135</h1>
-      <nav className="primary-nav">
-        <Link className="nav-pill" to="/">Home</Link>
-        <span className="nav-separator">||</span>
-        <Link className="nav-pill" to="/introduction">Introduction</Link>
-        <span className="nav-separator">||</span>
-        <Link className="nav-pill" to="/contract">Contract</Link>
-        <span className="nav-separator">||</span>
-        <Link className="nav-pill" to="/introductions">Introductions</Link>
-      </nav>
-    </>
-  );
+  return (<header>
+        <h1>Michael Cashions's Mighty Chicken || ITIS3135</h1>
+        <nav>
+            <p>
+            <Link to="/">Home</Link> || 
+            <Link to="/introduction"> Introduction</Link> || 
+            <Link to="/contract"> Contract</Link> ||
+            <Link to="/introductions"> Introductions</Link>
+            </p>
+           </nav>
+    </header>);
 }
