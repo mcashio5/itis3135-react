2a2569ab49a92c37de44a3a6beb7e8118f96bfcf
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 18:47:01 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router';
+import Introduction from './Introduction.jsx';
+import Layout from './Layout.jsx';
+import Contract from './Contract.jsx';
+import Students from './Students.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="/introduction" element={<Introduction />} />
+          <Route path="/contract" element={<Contract />} />
+          <Route path="/students" element={<Students />} />
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
