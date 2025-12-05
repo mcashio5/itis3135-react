5acba7e4c0861b1aca0461250cb3237fbba488b8
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 18:59:48 -0500

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,15 @@
 export default function Footer(){
     return (
         <footer>
-    <a href="https://webpages.charlotte.edu/abarne94/">CLT Web</a> || 
-        <a href="https://github.com/adambarnes917/adambarnes917.github.io">GitHub Profile</a> || 
-        <a href="https://adambarnes917.github.io/">GitHub.io</a>|| 
-        <a href="https://adambarnes917.github.io/itis3135/">GitHub.io 3135</a> || 
-        <a href="https://www.freecodecamp.org/adambarnes">FreeCodeCamp</a> || 
-        <a href="https://www.codecademy.com/profiles/adambarnes917">CodeAcademy</a> || 
-        <a href="https://www.linkedin.com/in/adam-barnes-7a78a9327/">LinkedIn</a>
-    <p><a href="mascot/">Angelic Bear</a></p>
-    <p>Designed by <a href="thebarnesdesign.com">Barnes Design ©2025</a></p>
+    <a href="https://webpages.charlotte.edu/mcashio5/">CLT Web</a> || 
+        <a href="https://github.com/mcashio5">GitHub Profile</a> || 
+        <a href="https://mcashio5.github.io">GitHub.io</a>|| 
+        <a href="https://mcashio5.github.io/itis3135/">GitHub.io 3135</a> || 
+        <a href="https://www.freecodecamp.org/mcashio5">FreeCodeCamp</a> || 
+        <a href="https://www.codecademy.com/profiles/mcashio5">CodeAcademy</a> || 
+        <a href="https://www.linkedin.com/in/michael-cashion-a891082b6/">LinkedIn</a>
+    <p><a href="https://webpages.charlotte.edu/mcashio5/itis3135/mightychicken/">Mighty Chicken</a></p>
+    <p>Designed by <a href="https://mcashio5.github.io/itis3135/cashioninc.com/">Cashion Inc. ©2025</a></p>
 </footer>
     );
 }
