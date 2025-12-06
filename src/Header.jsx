820a91d488dcba59d61a66f4eb94a9527ed615ef
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 19:41:12 -0500

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
-import Navbar from './Navbar.jsx';
+// src/Header.jsx
+import Navbar from "./Navbar";
+
 export default function Header() {
-    return (
-        <header>
-            <h1>Michael Cashion's Mighty Chicken ITIS 3135</h1>
-            <Navbar />
-        </header>
-    );
+  return (
+    <header className="site-header">
+      <h1>Michael Cashion's Mighty Chicken ITIS 3135</h1>
+      <Navbar />
+    </header>
+  );
 }
