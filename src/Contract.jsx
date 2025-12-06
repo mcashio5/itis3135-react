d4734eb3b606414ecf3fb742a3b033c846ae37cc
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 19:31:47 -0500

    Update Contract.jsx

--- src/Contract.jsx
+++ src/Contract.jsx
@@ -1,17 +1,46 @@
-import './App.css';
-export default function Contract(){
-    return (
-        <>
-            <h2>Contract</h2>
-        <p>I, Michael W. Cashion agree to abide by the terms of the course contract in my Fall 2025, ITIS 3135, Front-End Web App Development with my instructor, Professor D.I. von Briesen.</p>
-        <p>I understand that all work that I do on publicly available online tools will be available to the world, and will not put information there that is 
-             inappropriate for schoolwork, or that I wish to keep private.</p>
-        <p>I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.</p>
-        <p>I also understand that given the structure and content of this class it’s possible to find many examples online or even view my classmates’ code directly. I swear that I will only use these resources to learn, and will not cut and paste 
-              code except where I have properly given credit (i.e. external libraries) and never from my classmates.</p>
-        <p>Signed: Michael W. Cashion, 09/5/2025</p>
-        </>
-        
+// src/Contract.jsx
+import "./App.css";
 
-    );
+export default function Contract() {
+  return (
+    <section className="content-shell">
+      <div className="inner-card contract-card">
+        <h2>Contract</h2>
+
+        <p>
+          I, Michael W. Cashion, agree to abide by the terms of the course
+          contract in my Fall 2025, ITIS 3135, Front-End Web App Development
+          with my instructor, Professor D.I. von Briesen.
+        </p>
+
+        <p>
+          I understand that all work that I do on publicly available online
+          tools will be available to the world, and I will not put information
+          there that is inappropriate for schoolwork, or that I wish to keep
+          private.
+        </p>
+
+        <p>
+          I also understand that it is my work that counts for attendance, not
+          logins or showing up for class. Failure to turn in assignments may
+          show as absences.
+        </p>
+
+        <p>
+          I also understand that given the structure and content of this class,
+          it is possible to find many examples online or even view my classmates’
+          code directly. I swear that I will only use these resources to learn
+          and will not cut and paste code except where I have properly given
+          credit (i.e., external libraries) and never from classmates.
+        </p>
+
+        <p className="contract-signature">
+          Signed: <br />
+          <strong>Michael W. Cashion</strong> <br />
+          09/5/2025
+        </p>
+      </div>
+    </section>
+  );
 }
+
