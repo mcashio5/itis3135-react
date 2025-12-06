5ccdf669e29ec975f475504e238e36109a23c89c
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 19:07:08 -0500

    Update Introduction.jsx

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -5,25 +5,25 @@ export default function Introduction() {
 
         <>
         <h2>Introduction</h2>
-        <h3>Adam Barnes | Angelic Bear</h3>
-        <p>I acknowledge that any information I post on here will be public. AB 8/23/2025</p>
+        <h3>Michael Cashion | Mighty Chicken</h3>
+        <p>I acknowledge that any information I post on here will be public. MC 8/25/2025</p>
         <figure>    
-            <img src="./image-from-iOS.jpg" alt="Picture of me with my siblings in SF" />
-        <figcaption>Me with my siblings in San Francisco.</figcaption>
+            <img src="/michael-cashion-sm.jpg" alt="Picture of me a a fraternity meeting in 2024" />
+        <figcaption>Picture of me a a fraternity meeting in 2024</figcaption>
         </figure>
-        <p>I’m in my 2nd year at UNCC and I am majoring in computer science with a concentration in software engineering. I like to play basketball and go to the gym, and I am looking forward to meeting some of my classmates who share the same interests. </p>
+        <p>I am a computer science student working towards a bachelor of science in systems and networks administration. I enjoy watching football and fishing in my free time. I would like to graduate and one day open my own business after building enough capital.</p>
         <ul>
             <li>
-                <p>Personal Background: I was born and raised in Charlotte, North Carolina, but I have traveled all across the country with my family as my dad is an artist.</p>
+                <p>Personal Background: I am originally from Raleigh and am a huge Washington sports fan. I don't enjoy school and am ready to graduate and start working.</p>
             </li>
             <li>
-                <p>Professional Background: I worked for a couple of years at an indoor pool in Charlotte, and I am currently working an internship from home part time.</p>
+                <p>Professional Background:  I completed my second internship this summer at Teleflex Medical where I got exposure to many different fields of IT and how a network functions from the ground up as well as gaining a certification in Microsoft Azure.</p>
             </li>
             <li>
-                <p>Academic Background: I’m in my second year but am classified as a Junior this year.</p>
+                <p>Academic Background: I am a computer science major in my fourth year at UNCC.</p>
             </li>
             <li>
-                <p>Primary Computer: Lenovo, Windows 11, Laptop, my room</p>
+                <p>Primary Computer: I primarily use my Macbook Pro for most tasks.</p>
             </li>
             <li>
                 Courses I’m Taking & Why:
@@ -31,22 +31,28 @@ export default function Introduction() {
             <li >
             <ul>
                 <li>
-                    <p>ITIS-3135 Front-End Web App Development: I wanted to understand the behind the scenes of web development</p>
+                    <p>ITSC-2175-Logic and Algorithms: Required for my major.</p>
                 </li>
                 <li>
-                    <p>ITSC-3146 Intro to Operating Systems & Networking: I am required to take this course for my major.</p>
+                    <p>ITIS 3135 - Front-End Web Application Development: This course.</p>
                 </li>
                 <li>
-                    <p>ITSC-3155 Software Engineering: This course is in my concentration and I am curious to see how I like it.</p>
+                    <p>ITSC 3155 - Software Engineering: Required for systems and networking</p>
                 </li>
                 <li>
-                    <p>PSYC-1101L General Phychology Lab: I needed this course as I haven’t met all the gen-ed requirements for my degree.</p>
+                    <p>ITSC 3146 - Intro Operating Systems & Networking: It is a requirement for understanding systems and networks.</p>
                 </li>
                 <li>
-                    <p>STAT-2122 Intro to Prob & Stat: This is another course that I need, as I haven't gotten all of my math credits yet.</p> 
+                    <p>ITCS 3166 - Intro to Computer Networks: Required course for my major</p> 
                 </li>
             </ul>
             </li>
+            <p style={{textAlign: 'center'}}>"Whether you think you can, or you think you can't, you're right"<br/>
+           <i> - Henry Ford </i>
+            </p>
+            <hr/>
+            <p><small>I acknowledge that the information I present here is public. - M.C.</small></p>
+            <p><small>Date:<i>8/25/2025</i></small></p>
         </ul>
         </>
         
