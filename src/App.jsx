e799a973432b7cf35f89e4522003341a5f2124fe
Michael Cashion <mcashio5@uncc.edu>
2025-12-05 19:28:06 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+// src/App.jsx
+import "./App.css";
+
 export default function App() {
   return (
-    <>
-        <h2>Home</h2>
+    <section className="content-shell">
+      <div className="inner-card">
+        <h2>Welcome</h2>
         <p>This is all the content for my ITIS 3135 Class</p>
-    </>
+      </div>
+    </section>
   );
 }
